a64bba948b192dd0da785f6d96fa0c14a90fc4c8
Siya <siajabbarova@gmail.com>
2024-03-21 16:01:35 +0400

    Update DatePicker

--- src/components/title/Title.tsx
+++ src/components/title/Title.tsx
@@ -7,23 +7,31 @@ interface TitleProps {
 
 const Title: React.FC<TitleProps> = ({ addTask }) => {
     const [title, setTitle] = useState('');
+    const [date, setDate] = useState(new Date());
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setTitle(event.target.value);
     };
+    const handleChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setDate(new Date(event.target.value));
+    };
 
     const handleSubmit = () => {
         if (title.trim() !== '') {
-            addTask(title, new Date().toLocaleDateString());
+            addTask(title, date.toLocaleDateString());
             setTitle('');
+            setDate(new Date);
         }
     };
 
     return (
-        <section className="add-new-element-section">
-            <input id="task" type="text" placeholder="New Title..." value={title} onChange={handleChange}/>
-            <button className="add-btn" onClick={handleSubmit}>+</button>
-        </section>
+        <div className='new-task-sec'>
+            <input type='date' defaultValue={date.toISOString().substring(0,10)} onChange={handleChangeDate}/>
+            <section className="add-new-element-section">
+                <input id="task" type="text" placeholder="New Title..." value={title} onChange={handleChange}/>
+                <button className="add-btn" onClick={handleSubmit}>+</button>
+            </section>
+        </div>
     );
 };
 
